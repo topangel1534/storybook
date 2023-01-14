eae76e887313cb78c59a32bef07ef14aa7e36710
devmachine <matheusangel534@gmail.com>
2023-01-14 07:25:05 -0500

    Set two background

---
+++ src/components/generator/index.jsx
@@ -0,0 +1,11 @@
+import './index.scss';
+
+const Generator = () => {
+  return (
+    <div className="generator-container">
+      <h1>Here is the storybook</h1>
+    </div>
+  );
+};
+
+export default Generator;
--- src/layout/home/index.jsx
+++ src/layout/home/index.jsx
@@ -1,9 +1,12 @@
+import Generator from 'components/generator';
+
 import './index.scss';
 
 const Home = () => {
   return (
     <div className="home-container">
-      <div class="title">Storybook AI</div>
+      <div className="title">Storybook AI</div>
+      <Generator />
     </div>
   );
 };
