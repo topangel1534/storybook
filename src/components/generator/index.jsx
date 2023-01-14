135a53055bd587a7c35d3a6036d8266ca3f7f9f8
devmachine <matheusangel534@gmail.com>
2023-01-14 16:06:22 -0500

    Add generator form

--- src/components/generator/index.jsx
+++ src/components/generator/index.jsx
@@ -1,9 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWandMagicSparkles, faShuffle, faXmark } from '@fortawesome/free-solid-svg-icons';
+
 import './index.scss';
 
 const Generator = () => {
   return (
     <div className="generator-container">
-      <h1>Here is the storybook</h1>
+      <div className="generator-content">
+        <div className="keyword-form">
+          <input placeholder="Describe image here..." />
+          <FontAwesomeIcon icon={faXmark} />
+          <FontAwesomeIcon icon={faShuffle} />
+          <div className="generate-button">
+            <FontAwesomeIcon icon={faWandMagicSparkles} />
+            <p>GENERATE</p>
+          </div>
+        </div>
+      </div>
+      <div className="tool-box"></div>
     </div>
   );
 };
