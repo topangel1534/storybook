52fc9cb720439857f6c6140db7582779c7d69c32
devmachine <matheusangel534@gmail.com>
2023-01-19 21:29:04 -0500

    Update background on signup

--- src/components/signup/index.jsx
+++ src/components/signup/index.jsx
@@ -4,15 +4,17 @@ import './index.scss';
 const SignUp = () => {
   return (
     <div className="sign-up">
-      <div className="title">Storybook</div>
-      <div className="email-sign-btn">SIGN UP WITH EMAIL</div>
-      <div className="divider-caption">
-        <div></div>
-        <div>or sign up with</div>
-        <div></div>
-      </div>
-      <div className="google-sign-btn">
-        <img src={GoogleIcon} alt="Google icon" />
+      <div className="sign-up-container">
+        <div className="title">Storybook</div>
+        <div className="email-sign-btn">SIGN UP WITH EMAIL</div>
+        <div className="divider-caption">
+          <div></div>
+          <div>or sign up with</div>
+          <div></div>
+        </div>
+        <div className="google-sign-btn">
+          <img src={GoogleIcon} alt="Google icon" />
+        </div>
       </div>
     </div>
   );
