fd6a671946c237ffe2f7f87f8b5c6292d9e1894b
devmachine <matheusangel534@gmail.com>
2023-01-19 21:13:51 -0500

    Build signup component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Header from './layout/header';
 import Home from 'layout/home';
+import SignUp from 'components/signup';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/signup/index.jsx
@@ -0,0 +1,21 @@
+import GoogleIcon from '../../assets/images/gmail.png';
+import './index.scss';
+
+const SignUp = () => {
+  return (
+    <div className="sign-up">
+      <div className="title">Storybook</div>
+      <div className="email-sign-btn">SIGN UP WITH EMAIL</div>
+      <div className="divider-caption">
+        <div></div>
+        <div>or sign up with</div>
+        <div></div>
+      </div>
+      <div className="google-sign-btn">
+        <img src={GoogleIcon} alt="Google icon" />
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
