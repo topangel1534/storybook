31120340af5590ca11517664408ff25fc2f67b35
devmachine <matheusangel534@gmail.com>
2023-01-20 08:07:09 -0500

    Build signup form

--- src/components/signup/index.jsx
+++ src/components/signup/index.jsx
@@ -1,12 +1,17 @@
+import { useState, useEffect } from 'react';
 import GoogleIcon from '../../assets/images/gmail.png';
 import './index.scss';
 
 const SignUp = () => {
-  return (
-    <div className="sign-up">
-      <div className="sign-up-container">
+  const [emailFlag, setFlagEmail] = useState(false);
+  const [gmailFlag, setFlagGmail] = useState(false);
+  const InitialContent = () => {
+    return (
+      <>
         <div className="title">Storybook</div>
-        <div className="email-sign-btn">SIGN UP WITH EMAIL</div>
+        <div className="email-sign-btn" onClick={() => setFlagEmail(true)}>
+          SIGN UP WITH EMAIL
+        </div>
         <div className="divider-caption">
           <div></div>
           <div>or sign up with</div>
@@ -15,6 +20,39 @@ const SignUp = () => {
         <div className="google-sign-btn">
           <img src={GoogleIcon} alt="Google icon" />
         </div>
+      </>
+    );
+  };
+
+  const EmailContent = () => {
+    const [email, setEmail] = useState();
+    const [name, setName] = useState();
+    const [password, setPassword] = useState();
+
+    useEffect(() => {}, [email, name, password]);
+
+    const signUp = () => {
+      console.log(email, name, password);
+    };
+
+    return (
+      <div className="email-sign-form">
+        <div className="title">Storybook</div>
+        <input className="email-field" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+        <input className="name-field" placeholder="Name" onChange={(e) => setName(e.target.value)} />
+        <input className="password-field" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+        <div className="sign-up-button" onClick={signUp}>
+          SIGN UP
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="sign-up">
+      <div className="sign-up-container">
+        {!emailFlag && !gmailFlag && <InitialContent />}
+        {emailFlag && <EmailContent />}
       </div>
     </div>
   );
